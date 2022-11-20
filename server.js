557827b9597b49177400494c968ba3f0eb812633
amanchy <aman@portpro.io>
2022-11-20 13:33:52 +0545

    Add :port

--- server.js
+++ server.js
@@ -2,11 +2,12 @@ const express = require("express");
 const app = express();
 const morgan = require("morgan");
 
+const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(morgan("tiny"));
 
 app.use("/", require("./routes/routes"));
 
-app.listen(5000, () => {
+app.listen(PORT, () => {
   console.log("Server running in port 5000");
 });
