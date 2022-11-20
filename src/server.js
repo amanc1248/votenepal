bddadba305f587307cd9b91f01f3729248fbd1cb
amanc1248 <amanc1248@gmail.com>
2022-11-20 16:01:02 +0545

    Add: netlify

--- src/server.js
+++ src/server.js
@@ -1,14 +1,15 @@
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
-
+const serverless =  require('serverless-http')
 // h
 const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(morgan("tiny"));
 
-app.use("/", require("./routes/routes"));
+app.use("/.netlify/functions/api", require("../routes/routes"));
 
 app.listen(PORT, () => {
   console.log("Server running in port 5000");
 });
+module.exports.handler = serverless(app);
